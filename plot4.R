dc65995df4f23ed16984cea68c529e83cc19de08
Cheryl <cjerozal@atlassian.com>
2014-07-24 07:22:45 -0700

    finished plot 4

--- plot4.R
+++ plot4.R
@@ -1,19 +1,19 @@
 # 4. Across the United States, how have emissions from coal combustion-related
 # sources changed from 1999–2008?
 
-# TODO lib import
+library(ggplot2)
 
-# # read in data
-# NEI <- readRDS("exdata_data_NEI_data/summarySCC_PM25.rds")
-# SCC <- readRDS("exdata_data_NEI_data/Source_Classification_Code.rds")
-# 
-# # find SCC ids for coal combustion sources
-# SCC_ids <- vector(mode = "character")
-# apply(SCC, 1, function(row) {
-#     if (grepl("Coal", row[["Short.Name"]], fixed = TRUE)) {
-#         SCC_ids <<- append(SCC_ids, row[["SCC"]])
-#     }
-# })
+# read in data
+NEI <- readRDS("exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("exdata_data_NEI_data/Source_Classification_Code.rds")
+
+# find SCC ids for coal combustion sources
+SCC_ids <- vector(mode = "character")
+apply(SCC, 1, function(row) {
+    if (grepl("Coal", row[["Short.Name"]], fixed = TRUE)) {
+        SCC_ids <<- append(SCC_ids, row[["SCC"]])
+    }
+})
 
 # Create a function to separate printing to a PNG from creating the plot
 outputToPNG <- function(fileName, createPlot) {
@@ -23,11 +23,10 @@ outputToPNG <- function(fileName, createPlot) {
 }
 
 createPlot <- function() {
-    print(class(SCC_ids)) ## TODO remove
-    ## remove data with SCCs that don't deal with coal
+
+    # remove data with SCCs that don't deal with coal
     coalSCCs <- NEI[, "SCC"] %in% SCC_ids
     coalNEI <- NEI[coalSCCs, ]
-    print("hi2") ## TODO remove
     
     # calculate the total emissions by year
     years <- vector(mode = "character")
@@ -40,20 +39,20 @@ createPlot <- function() {
         emissions <- append(emissions, yearEmissions)
     }
     
-    # TODO change to thousands of tons
-    
     # construct the data frame and plot it
     emissionData <- data.frame(years, emissions)
-    g <- qplot(years, emissions, data = emissionData #,
-               #ylim = c(0, 2500)  # start the axis at 0 since emission totals cannot be negative
-    )
+    emissionData$emissionsInThousandsOfTons <-
+        apply(emissionData, 1, function(row) {
+            as.numeric(row[["emissions"]])/1000
+        })
+    g <- qplot(years, emissionsInThousandsOfTons, data = emissionData)
     print(g + geom_point()
           + geom_smooth(mapping = aes(group = 1), method = "lm")
-          + labs(title = expression('Baltimore PM'[2.5]*' Emissions by Type and Year'), # TODO change
-                 x = "",
-                 y = "Emissions (tons)")
+          + labs(title = expression('U.S. Emissions from Coal Combustion'),
+                 x = "Year",
+                 y = "Emissions (thousands of tons)")
           + theme_bw(base_family = "Times"))
 }
 
 createPlot()
-#outputToPNG("plot4.png", createPlot)
+outputToPNG("plot4.png", createPlot)
