9e01c731dd2f3175bb5c7d74572def36a7606b4e
Cheryl <cjerozal@atlassian.com>
2014-07-24 07:23:58 -0700

    adding plot 5 code

---
+++ plot5.R
@@ -0,0 +1,49 @@
+# 5. How have emissions from motor vehicle sources changed from 1999–2008 in
+# Baltimore City?
+
+library(ggplot2)
+
+# read in data
+NEI <- readRDS("exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("exdata_data_NEI_data/Source_Classification_Code.rds")
+
+# filter down to only motor vehicle data from Baltimore City, Maryland
+baltimoreRows <- NEI[, "fips"] == "24510"
+filteredNEI <- NEI[baltimoreRows, ]
+onroadRows <- filteredNEI[, "type"] == "ON-ROAD"
+twiceFilteredNEI <- filteredNEI[onroadRows, ]
+
+# Create a function to separate printing to a PNG from creating the plot
+outputToPNG <- function(fileName, createPlot) {
+    png(file = fileName)
+    createPlot()
+    dev.off()
+}
+
+createPlot <- function() {
+
+    # calculate the total emissions by year
+    years <- vector(mode = "character")
+    emissions <- vector(mode = "numeric")
+    split_NEI <- split(twiceFilteredNEI, twiceFilteredNEI$year)
+    for(year in names(split_NEI)) {
+        yearDataFrame <- split_NEI[[year]]
+        yearEmissions <- sum(yearDataFrame$Emissions)
+        years <- append(years, year)
+        emissions <- append(emissions, yearEmissions)
+    }
+    
+    # construct the data frame and plot it
+    emissionData <- data.frame(years, emissions)
+    g <- qplot(years, emissions, data = emissionData,
+               ylim = c(0, 400))  # start the axis at 0 since emission totals cannot be negative)
+    print(g + geom_point()
+          + geom_smooth(mapping = aes(group = 1), method = "lm")
+          + labs(title = expression('Motor Vehicle Emissions in Baltimore City'),
+                 x = "Year",
+                 y = "Emissions (tons)")
+          + theme_bw(base_family = "Times"))
+}
+
+createPlot()
+outputToPNG("plot5.png", createPlot)
